c522a958a77f428349de030341d4a35cfa17e48b
augustyniakpiotr3@gmail.com <augustyniakpiotr3@gmail.com>
2022-12-13 22:24:11 +0100

    homework 3

--- Homework3.py
+++ Homework3.py
@@ -3,15 +3,15 @@ import os
 def files_count(my_folder = r'C:\python_hw_folder'):
     # It will store the number of files and the total size occupied respectively
     number_of_files, overall_size = 0, 0
-    for fileName in os.listdir(my_folder):
-        currentFile = os.path.join(my_folder, fileName)
+    for file_name in os.listdir(my_folder):
+        current_file = os.path.join(my_folder, file_name)
         # Here, if currentFile is a file, add 1 to number_of_files and add the file size to overall_size
-        if os.path.isfile(currentFile):
+        if os.path.isfile(current_file):
             number_of_files = number_of_files + 1
-            overall_size = overall_size + os.path.getsize(currentFile)
+            overall_size = overall_size + os.path.getsize(current_file)
         #else recursively call the same function on the subfolder
         else:
-            [temp1, temp2] = files_count(currentFile)
+            [temp1, temp2] = files_count(current_file)
             number_of_files = number_of_files + temp1
             overall_size = overall_size + temp2
     return [number_of_files, overall_size]
